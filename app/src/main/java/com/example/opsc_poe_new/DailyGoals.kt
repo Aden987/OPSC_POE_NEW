5b8055665903846f675bf6753ded4ad252185329
ChazCrypto <chazrichardson50@gmail.com>
2023-09-28 20:38:31 +0200

    Max and Min Goals

---
+++ app/src/main/java/com/example/opsc_poe_new/DailyGoals.kt
@@ -0,0 +1,61 @@
+package com.example.opsc_poe_new
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class DailyGoals : AppCompatActivity() {
+    lateinit var  BackButton : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_daily_goals)
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+        val minGoalEditText = findViewById<EditText>(R.id.editTextMinGoal)
+        val maxGoalEditText = findViewById<EditText>(R.id.editTextMaxGoal)
+        val saveButton = findViewById<Button>(R.id.saveButton)
+
+        // Retrieve user's goals from SharedPreferences
+        val minGoal = sharedPreferences.getFloat("minGoal", 0.0f)
+        val maxGoal = sharedPreferences.getFloat("maxGoal", 0.0f)
+
+        // Display the retrieved goals in EditText fields
+        minGoalEditText.setText(minGoal.toString())
+        maxGoalEditText.setText(maxGoal.toString())
+
+        saveButton.setOnClickListener {
+            // Get user's input from EditText fields
+            val minGoalText = minGoalEditText.text.toString()
+            val maxGoalText = maxGoalEditText.text.toString()
+
+            // Convert input to float, default to 0.0 if conversion fails
+            val minGoalValue = minGoalText.toFloatOrNull() ?: 0.0f
+            val maxGoalValue = maxGoalText.toFloatOrNull() ?: 0.0f
+
+            // Validate user's input (optional)
+            if (minGoalValue < 0.0 || maxGoalValue < 0.0 || minGoalValue > maxGoalValue) {
+                // Display an error message to the user
+                // Prompt the user to correct their input
+                Toast.makeText(this, "Invalid input. Please check your values.", Toast.LENGTH_SHORT).show()
+            } else {
+                // Save the valid input to SharedPreferences
+                val editor = sharedPreferences.edit()
+                editor.putFloat("minGoal", minGoalValue)
+                editor.putFloat("maxGoal", maxGoalValue)
+                editor.apply()
+
+                // Notify the user that the goals are saved
+                Toast.makeText(this, "Goals saved successfully.", Toast.LENGTH_SHORT).show()
+            }
+        }
+        BackButton = findViewById(R.id.back)
+        BackButton.setOnClickListener({
+            val intent = Intent(this,SelectPage::class.java)
+            startActivity(intent)}
+        )
+    }
+}
--- app/src/main/java/com/example/opsc_poe_new/SelectPage.kt
+++ app/src/main/java/com/example/opsc_poe_new/SelectPage.kt
@@ -9,12 +9,19 @@ import android.widget.Button
 class SelectPage : AppCompatActivity() {
 
     lateinit var  timesheetBtn : Button
+    lateinit var  GoalButton : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.selection_page)
         timesheetBtn = findViewById(R.id.timesheetBtn)
         timesheetBtn.setOnClickListener({
             val intent = Intent(this,TimesheeyEntry::class.java)
+            startActivity(intent)}
+        )
+        GoalButton = findViewById(R.id.goalButton)
+        GoalButton.setOnClickListener({
+            val intent = Intent(this,DailyGoals::class.java)
             startActivity(intent)}
         )
     }
