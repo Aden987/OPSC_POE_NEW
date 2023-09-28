b43740345edad175a0e6b06c58807b10c0e85a8d
Michael Stewart <michaelandrewstew@gmail.com>
2023-09-28 13:02:13 +0200

    busy working on it

--- app/src/main/java/com/example/opsc_poe_new/MainActivity.kt
+++ app/src/main/java/com/example/opsc_poe_new/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.opsc_poe_new
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var username : EditText
+    lateinit var password : EditText
+    lateinit var  loginButton : Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.login)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+
+        if(loginButton.isActivated)
+        {
+            if(username.text.toString() = "user")
+            {
+                setContentView(R.layout.selection_page)
+            }
+        }
+
     }
 }
