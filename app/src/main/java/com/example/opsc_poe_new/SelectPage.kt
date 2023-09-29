2ffc4f1b2a090ca020f268471cd32107c0dc9802
Aden987 <naidoo.aden@gmail.com>
2023-09-29 11:12:15 +0200

    chazs daily goal code

--- app/src/main/java/com/example/opsc_poe_new/SelectPage.kt
+++ app/src/main/java/com/example/opsc_poe_new/SelectPage.kt
@@ -9,12 +9,18 @@ import android.widget.Button
 class SelectPage : AppCompatActivity() {
 
     lateinit var  timesheetBtn : Button
+    lateinit var  dailyGoalBtn : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.selection_page)
         timesheetBtn = findViewById(R.id.timesheetBtn)
         timesheetBtn.setOnClickListener({
             val intent = Intent(this,TimesheeyEntry::class.java)
+            startActivity(intent)}
+        )
+        dailyGoalBtn = findViewById(R.id.goalBtn)
+        dailyGoalBtn.setOnClickListener({
+            val intent = Intent(this,DailyGoal::class.java)
             startActivity(intent)}
         )
     }
