7002798fe4bd228af53c1db6ede26653f7fc8bf9
Aden987 <naidoo.aden@gmail.com>
2023-12-02 02:29:51 +0200

    line graph 2

--- app/src/main/java/com/example/opsc_poe_new/TimesheetGraph.kt
+++ app/src/main/java/com/example/opsc_poe_new/TimesheetGraph.kt
@@ -6,6 +6,7 @@ import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 
 
 class TimesheetGraph : AppCompatActivity() {
@@ -22,23 +23,41 @@ class TimesheetGraph : AppCompatActivity() {
     fun setLineChartData()
     {
         val xvalue = ArrayList<String>()
-        xvalue.add("11:00 AM")
-        xvalue.add("12:00 AM")
-        xvalue.add("1:00 PM")
-        xvalue.add("3:00 PM")
-        xvalue.add("4:00 PM")
+        xvalue.add("Sun")
+        xvalue.add("Mon")
+        xvalue.add("Tue")
+        xvalue.add("Wed")
+        xvalue.add("Thurs")
+        xvalue.add("Fri")
+        xvalue.add("Sat")
 
         val lineentry = ArrayList<Entry>()
-        lineentry.add(Entry(20f, 0))
-        lineentry.add(Entry(50f,1))
-        lineentry.add(Entry(60f,2))
-        lineentry.add(Entry(30f,3))
-        lineentry.add(Entry(10f,4))
+        lineentry.add(Entry(1.5f, 0))
+        lineentry.add(Entry(3.5f,1))
+        lineentry.add(Entry(8.5f,2))
+        lineentry.add(Entry(4.5f,3))
+        lineentry.add(Entry(5.5f,4))
+
+        val lineentry1 = ArrayList<Entry>()
+        lineentry1.add(Entry(0.5f, 0))
+        lineentry1.add(Entry(0.5f,1))
+        lineentry1.add(Entry(0.5f,2))
+        lineentry1.add(Entry(0.5f,3))
+        lineentry1.add(Entry(0.5f,4))
 
         val linedataset = LineDataSet(lineentry, "First")
         linedataset.color = resources.getColor(R.color.green)
 
-        val data = LineData(xvalue,linedataset)
+        val linedataset1 = LineDataSet(lineentry1, "Second")
+        linedataset.color = resources.getColor(R.color.blue)
+
+        val finaldataset = ArrayList<LineDataSet>()
+        finaldataset.add(linedataset)
+        finaldataset.add(linedataset1)
+
+
+
+        val data = LineData(xvalue, finaldataset as List<ILineDataSet>?)
         lineChart?.data = data
     }
 }
